7dab73852d8e887791e41817cc01e98df900a12c
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2024-05-07 23:14:53 +0200

    u

--- scrape_unraid.py
+++ scrape_unraid.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 from bs4 import BeautifulSoup
+import requests
 
 URL = 'http://192.168.178.132/Apps'
 TEMPLATE = f'{URL}/AddContainer?xmlTemplate=default:'
@@ -11,23 +12,17 @@ driver.get(URL)
 sleep(1)
 
 # Login
+per_page = 24
 inputs = driver.find_elements(By.TAG_NAME, "input")
 inputs[0].send_keys("root")
 inputs[1].send_keys("Bazinga!73")
 driver.find_element(By.TAG_NAME, "button").click()
 
-driver.get(URL)
-sleep(1)
-driver.find_element(By.CLASS_NAME, "homeMore").click()
-sleep(1)
-
-containers = []
-for i in range(1, 2):
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    container_links = soup.find_all("div", class_="ca_holder spotlightHome ca_appPopup")
-    container_links = [TEMPLATE+l.get("data-apppath") for l in container_links]
-    containers.extend(container_links)
+for i in range(1,2):
+    driver.get(URL)
+    sleep(3)
+    driver.find_element(By.CLASS_NAME, "homeMore").click()
+    sleep(1)
     driver.execute_script(f'changePage("{i}")')
 
-print(containers)
-driver.execute_script("popupInstallXML('/tmp/community.applications/tempFiles/templates-community-apps/GrafGenixsRepository/androidseb25-iGotify-latest.xml','default','','')")
+
---
+++ unraid_xml_coverter.py
@@ -0,0 +1,53 @@
+import os
+from bs4 import BeautifulSoup
+
+
+xml_dir = 'xml'
+config_dir = 'containers'
+xml_files = os.listdir(xml_dir)
+
+for xml_file in xml_files:
+    with open(f'{xml_dir}/{xml_file}', 'r') as f:
+        soup = BeautifulSoup(f.read(), 'xml')
+    name = soup.find('Name').text
+    repo = soup.find('Repository').text
+    parameters = soup.find_all('Config')
+    ports = [{'name': p.get('Name').upper().replace(' ','_'),'target':p.get('Target'), 'value':p.get('Default'), 'mode': p.get('Mode')} for p in parameters if p.get('Type') == 'Port']
+    variables = [{'target':p.get('Target'), 'value':p.get('Default')} for p in parameters if p.get('Type') == 'Variable']
+    volumes = [{'name': p.get('Name').upper().replace(' ','_'), 'target':p.get('Target'), 'value':p.get('Default')[9:]} for p in parameters if p.get('Type') == 'Path']
+
+    run_cmd = 'COMMAND="docker run -d --name={{NAME}} '
+    for port in ports:
+        run_cmd += f'-p {{{{{port["name"]}}}}}:{port["target"]}/{port["mode"]} '
+    for volume in volumes:
+        run_cmd += f'-v {{{{{volume["name"]}}}}}:{volume["target"]} '
+    for variable in variables:
+        if variable["target"] != 'UMASK' and variable["target"] != 'PUID' and variable["target"] != 'PGID':
+            run_cmd += f'-e {variable["target"]}={{{{{variable["target"]}}}}} '
+        else:
+            run_cmd += f'-e {variable["target"]}={variable["value"]} '
+    file_content = f'{run_cmd} {repo}"\n'
+
+
+
+
+    for p in ports:
+        if p['value'] == '':
+            p['value'] = 'CUSTOM_VALUE'
+        file_content += f'{p["name"]}={p["value"]}\n'
+
+
+    for v in variables:
+        if ' ' in v['value']:
+            v['value'] = f'"{v["value"]}"'
+        if v['value'] == '':
+            v['value'] = 'CUSTOM_VALUE'
+        if v["target"] != 'UMASK' and v["target"] != 'PUID' and v["target"] != 'PGID':
+            file_content += f'{v["target"]}={v["value"]}\n'
+    for v in volumes:
+        if v['value'] == '':
+            v['value'] = f'/appdata/{name}'
+        file_content += f'{v["name"]}={v["value"]}\n'
+
+    with open(f'{config_dir}/{name.lower()}.conf', 'w') as f:
+        f.write(file_content)
